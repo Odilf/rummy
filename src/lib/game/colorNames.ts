84e6d4794b34cd319c17aead37a14f6071fecee0
Odilf <odysseas.maheras@gmail.com>
2021-12-28 00:40:48 +0200

    made color names use degfaults

--- src/lib/game/colorNames.ts
+++ src/lib/game/colorNames.ts
@@ -1,3 +1,10 @@
 import { writable } from "svelte/store";
 
-export let colorNames = writable({})
+export let colorNames = writable({
+	"80a659": "Springview Green",
+	"59a6a6": "Paradise Island",
+	"8059a6": "Highlighter Lavender",
+	"a65959": "Cheek Red"
+})
+
+colorNames.subscribe(console.log)
