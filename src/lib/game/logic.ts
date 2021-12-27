87dfb09874a2c073520c4345004f8053ac8a37cb
Odilf <odysseas.maheras@gmail.com>
2021-12-27 21:58:28 +0200

    fixed range bug

--- src/lib/game/logic.ts
+++ src/lib/game/logic.ts
@@ -107,7 +107,7 @@ export class Game {
 
 function range(a: number, b: number = null): number[] {
 	const size = b == null ? a : (b - a)
-	return [...Array(size).keys()]
+	return [...Array(size).keys()].map(i => i + a)
 }
 
 export function isStair(set: Token[]): boolean {
