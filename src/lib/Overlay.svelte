98db6c9d3831b364d758bb18dfaee6a621e98132
Odilf <odysseas.maheras@gmail.com>
2021-12-28 12:22:08 +0200

    added an Overlay component

---
+++ src/lib/Overlay.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { fade } from "svelte/transition";
+	export let disabled = false
+</script>
+
+{#if !disabled}	
+<main 
+class='fixed inset-0 bg-black/70 z-20 backdrop-blur w-full justify-center'
+transition:fade
+>
+	<slot/>
+</main>
+{/if}
