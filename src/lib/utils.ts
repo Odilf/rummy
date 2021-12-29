48745b88ae77721b1cd5adf697df12aafe261dbd
Odilf <odysseas.maheras@gmail.com>
2021-12-29 09:35:28 +0200

    moved `shuffleArray` to `utils.ts`

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,3 +3,12 @@ export function range(a: number, b: number = null): number[] {
 	const adder = b == null ? 0 : a
 	return [...Array(size).keys()].map(i => i + adder)
 }
+
+function shuffleArray<T>(array: Array<T>) {
+	array = [...array]
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]]
+	}
+	return array
+}
