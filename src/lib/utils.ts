4c53fb43bc0422ed91011b9e433e9ca5dda891b8
Odilf <odysseas.maheras@gmail.com>
2022-01-01 09:39:41 +0200

    created assignifNull util function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,11 +4,15 @@ export function range(a: number, b: number = null): number[] {
 	return [...Array(size).keys()].map(i => i + adder)
 }
 
-function shuffleArray<T>(array: Array<T>) {
+export function shuffleArray<T>(array: Array<T>) {
 	array = [...array]
 	for (let i = array.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
 		[array[i], array[j]] = [array[j], array[i]]
 	}
 	return array
+}
+
+export function assignIfNull(object: any, value: any) {
+	return object == null ? value : object
 }
