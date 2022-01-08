6aeff022df92bee9fb04e2ede8b48f5b02fdbda1
Odilf <odysseas.maheras@gmail.com>
2022-01-08 12:58:32 +0200

    animate board sets

--- src/lib/game/Board.svelte
+++ src/lib/game/Board.svelte
@@ -7,10 +7,10 @@
 	export let sets: Token[][]
 </script>
 
-<div class='bg-tertiary min-h-[300px] h-fit w-4/5 rounded-lg flex flex-wrap px-10 py-2 sm:py-5 justify-center shadow-inner' 
+<div class='bg-tertiary min-h-[300px] h-fit w-full sm:w-4/5 rounded-lg flex flex-wrap px-0 py-2 sm:py-5 justify-center shadow' 
 use:dropzone on:drop>
 	{#each sets as set (set[0].index)}
-		<div animate:flip> 
+		<div> 
 			<Set tokens={set} index={set[0].index} on:drop/>
 		</div>
 	{/each}
