fdbc747f00053fc694dee56263c30b41a83e92c8
Odilf <odysseas.maheras@gmail.com>
2022-01-08 18:45:02 +0200

    able to return owns' hand

--- src/lib/online/OnlineGame.svelte
+++ src/lib/online/OnlineGame.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
 	import { Place, Token } from "$lib/game/logic/token";
 	import Hand from "$lib/game/Hand.svelte";
-	import { getBoard, getHand, lowestUnusedIndex } from "$lib/game/logic/game";
+	import { draw, getBoard, getHand, lowestUnusedIndex } from "$lib/game/logic/game";
 	import Board from "$lib/game/Board.svelte";
 	import { isValidBoard } from "$lib/game/logic/set";
-	import { requestDraw, updateTokens } from "$lib/database";
+	import { updateTokens } from "$lib/database";
+	import { createSnackbar } from "$lib/utils/snackbar";
+import { clone } from "$lib/utils";
 
 	export let tokens: Token[] = []
 	export let index: number
 	export let uid: string
+	export let drawDelay: number
+
+	let lastValidState: Token[] = clone(tokens)
+	let canDraw = true
+	let insistCount = 0
+
+	$: console.log('last valid state', lastValidState, index, getHand(lastValidState, index))
 
 	console.log(tokens);
 
@@ -16,33 +25,83 @@
 		if ('token' in e.detail) {
 			const tokenData = e.detail.token
 
-			let index: number
+			let tokenIndex: number
 			let place = Place.Board
 			if ('setIndex' in e.detail) {
-				index = e.detail.setIndex
+				tokenIndex = e.detail.setIndex
 			} else if ('playerIndex' in e.detail) {
-				index = e.detail.playerIndex
+				if (!getHand(lastValidState, index).map(token => token.id).includes(tokenData.id)) {
+					console.log('Not my token!', tokenData, tokenIndex, getHand(lastValidState, index).map(token => token.id), lastValidState);
+					return
+				}
+
+				tokenIndex = e.detail.playerIndex
 				place = Place.Hand
 			} else {
-				index = lowestUnusedIndex(tokens)
+				tokenIndex = lowestUnusedIndex(tokens)
 			}
 
 			const token = tokens.find(token => token.id === tokenData.id)
 			token.belongs = place
-			token.index = index
+			token.index = tokenIndex
 
 			tokens = tokens
 		}
 
 		if (isValidBoard(getBoard(tokens))) {
+			lastValidState = clone(tokens)
 			updateTokens(uid, tokens)
+		}
+	}
+
+	function attemptDraw() {
+		if (canDraw) {
+			updateTokens(uid, draw(tokens, index))
+			canDraw = false
+			setTimeout(() => {
+				canDraw = true
+				insistCount = 0
+			}, drawDelay)
+		} else {
+			insistCount += 1
+			if (insistCount < 3) {
+				createSnackbar( { message: 'Cooldown hasn\'t finished', lifetime: 1000 })
+			} else if (insistCount === 3) {
+				createSnackbar( { message: 'Yoooo bro chill for a moment', lifetime: 2500 })
+			}
 		}
 	}
 	
 </script>
 
-<button on:click={() => requestDraw(uid, index)}> Request draw </button>
+<svelte:window on:keydown={e => e.code === 'Space' && attemptDraw()}/>
 
-<Hand hand={getHand(tokens, index)} {index} on:drop={handleDrop}/>
+<div class='flex flex-col items-center pb-32'>
+	<div class='sticky top-2 z-10 bg-secondary p-4 m-2 rounded-xl overflow-hidden shadow'>
+		<button on:click={attemptDraw}>
+			Draw
+		</button>
 
-<Board sets={getBoard(tokens)} on:drop={handleDrop}/>
+		<div class='absolute bg-tertiary/50 pointer-events-none {canDraw ? '' : 'reload'}' style='--drawDelay: {drawDelay}ms'/> 
+	</div>
+	
+	<Hand hand={getHand(tokens, index)} {index} on:drop={handleDrop}/>
+	
+	<Board sets={getBoard(tokens)} on:drop={handleDrop}/>
+</div>
+
+<style>
+	.reload {
+		inset: 0;
+		animation: load var(--drawDelay) ease;
+	}
+	@keyframes load {
+		from {
+			top: 0px;
+		}
+
+		to {
+			top: 100%;
+		}
+	}
+</style>
